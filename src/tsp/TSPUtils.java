8fc5a1359027d1f02b4bef8cc67db1bb219a73c2
Syed Ali Rizwan <alirizwan2003@gmail.com>
2023-12-10 21:54:06 +0500

    first commit

--- src/tsp/TSPUtils.java
+++ src/tsp/TSPUtils.java
@@ -3,8 +3,6 @@ package tsp;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -52,21 +50,6 @@ public class TSPUtils {
 
     static int randomIndex(final int limit) {
         return R.nextInt(limit);
-    }
-
-    static<T> List<T>[] split(final List<T> list) {
-        final List<T> first = new ArrayList<>();
-        final List<T> second = new ArrayList<>();
-        final int size = list.size();
-        final int partitionIndex = 1 + TSPUtils.randomIndex(list.size());
-        IntStream.range(0, size).forEach(i -> {
-            if (i < (size + 1) / partitionIndex) {
-                first.add(list.get(i));
-            } else {
-                second.add(list.get(i));
-            }
-        });
-        return (List<T>[]) new List[]{first, second};
     }
 }
 
